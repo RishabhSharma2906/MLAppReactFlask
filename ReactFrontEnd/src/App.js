71257128fbd78b2edd69f6128f92c02843ad1555
RishabhSharma2906 <rishabh.coding@gmail.com>
2018-09-23 07:17:02 -0700

    Adding react front end

---
+++ ReactFrontEnd/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Review Decoder</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <footer className="App-footer">
+          <h1 className="App-title">Created by Rishabh</h1>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
